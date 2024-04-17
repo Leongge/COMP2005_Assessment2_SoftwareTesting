3322c4f5620980135a291490a1470ccdaeec712c
leongge <leong10722@gmail.com>
2024-04-17 17:30:59 +0800

    complete f2 endpoint function

--- src/main/java/com/leong/SDTP/controller/ApiController.java
+++ src/main/java/com/leong/SDTP/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.leong.SDTP.controller;
 
 import com.leong.SDTP.model.Admission;
+import com.leong.SDTP.model.Patient;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -10,9 +11,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,5 +69,69 @@ public class ApiController {
         }
 
         return admissions;
+    }
+
+    @PostMapping("/f2")
+    public List<Patient> filterAdmissions(@RequestBody String dateJson) {
+        List<Patient> admittedPatients = new ArrayList<>();
+
+        try {
+            // Parse the JSON input
+            JSONObject dateObject = new JSONObject(dateJson);
+            String date = dateObject.getString("date");
+
+            // Fetch admissions data
+            String admissionsUrl = "https://web.socem.plymouth.ac.uk/COMP2005/api/Admissions";
+            HttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(admissionsUrl);
+            HttpResponse response = httpClient.execute(httpGet);
+
+            if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
+                String admissionsResponse = EntityUtils.toString(response.getEntity());
+                JSONArray admissionsArray = new JSONArray(admissionsResponse);
+
+                // Iterate through admissions
+                for (int i = 0; i < admissionsArray.length(); i++) {
+                    JSONObject admissionObject = admissionsArray.getJSONObject(i);
+                    String dischargeDate = admissionObject.getString("dischargeDate");
+
+                    // Check if discharge date is after the input date
+                    if (dischargeDate.compareTo(date) > 0) {
+                        int patientID = admissionObject.getInt("patientID");
+
+                        // Fetch patient data using patient ID
+                        String patientsUrl = "https://web.socem.plymouth.ac.uk/COMP2005/api/Patients";
+                        httpGet = new HttpGet(patientsUrl);
+                        response = httpClient.execute(httpGet);
+
+                        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
+                            String patientsResponse = EntityUtils.toString(response.getEntity());
+                            JSONArray patientsArray = new JSONArray(patientsResponse);
+
+                            // Iterate through patients
+                            for (int j = 0; j < patientsArray.length(); j++) {
+                                JSONObject patientObject = patientsArray.getJSONObject(j);
+                                if (patientObject.getInt("id") == patientID) {
+                                    String surname = patientObject.getString("surname");
+                                    String forename = patientObject.getString("forename");
+                                    String nhsNumber = patientObject.getString("nhsNumber");
+                                    Patient patient = new Patient(patientID, surname, forename, nhsNumber);
+                                    admittedPatients.add(patient);
+                                    break;
+                                }
+                            }
+                        } else {
+                            System.err.println("Failed to fetch patients data");
+                        }
+                    }
+                }
+            } else {
+                System.err.println("Failed to fetch admissions data");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return admittedPatients;
     }
 }
